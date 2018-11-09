e398bdb9853f32ed06884e6a3d70043803f1ddab
Serg <beherit1977@gmail.com>
2018-11-09 10:55:14 +0000

    added player class

--- player.rb
+++ player.rb
@@ -1,2 +1,14 @@
 class Player
+  attr_reader :name, :bank
+
+  # по умолчанию создается Дилер
+  def initialize(name = 'Dealer')
+    @name = name
+    @bank = 100
+  end
+
+  # сделать ставку
+  def make_bet(value = 10)
+    @bank -= value
+  end
 end
