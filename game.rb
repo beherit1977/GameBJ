76cf89495006465c7a32d58fda73c731e6bad420
Serg <beherit1977@gmail.com>
2018-11-13 07:06:20 +0000

    added game_result counting and exposing

--- game.rb
+++ game.rb
@@ -9,7 +9,7 @@ class Game
   end
 
   def assign_players(interface)
-    @human = Player.new(@interface.greeting)
+    @human = Player.new(interface.greeting)
     @comp = Player.new
   end
 
@@ -20,12 +20,6 @@ class Game
 
   def black_jack?
     (@human.count_points == 21) || (@comp.count_points == 21)
-  end
-
-  def game_result
-    @game_over = true
-    @interface.show_player_deal(@human)
-    @interface.show_comp_deal(@comp, @game_over)
   end
 
   def deal(number_of_cards)
@@ -57,10 +51,51 @@ class Game
   def dealer_turn
     if @comp.count_points < 17
       @comp.add_cards(deal(1))
-      @interface.show_comp_deal(@comp, @game_over)
-    else
-      @interface.show_comp_deal(@comp, @game_over)
+      @interface.show_dealer_hit
       game_result
+    else
+      @interface.show_dealer_stay
+      game_result
+    end
+  end
+
+  def game_result
+    @game_over = true
+    @interface.show_game_over
+    @interface.show_player_deal(@human)
+    @interface.show_comp_deal(@comp, @game_over)
+    count_result
+  end
+
+  def count_result
+    if (@human.count_points > 21) && (@comp.count_points > 21)
+      @interface.show_lose
+      return
+    elsif @human.count_points == @comp.count_points
+      @interface.show_tie
+    else
+      game_win
+    end
+  end
+
+  def game_win
+    if black_jack?
+      @interface.show_bj_win
+      choose_winner
+    elsif
+      choose_winner
+    end
+  end
+
+  def choose_winner
+    if @human.count_points > 21
+      @interface.show_comp_win(@comp)
+    elsif @comp.count_points > 21
+      @interface.show_human_win(@human)
+    elsif @human.count_points > @comp.count_points
+      @interface.show_human_win(@human)
+    else
+      @interface.show_comp_win(@comp)
     end
   end
 end
--- interface.rb
+++ interface.rb
@@ -11,11 +11,11 @@ class Interface
   end
 
   def show_dealer_points(comp)
-    puts "Внимание! У дилера #{comp.count_points} очков"
+    puts "Внимание! У Дилера #{comp.count_points} очков"
   end
 
   def show_player_deal(human)
-    puts "У Игрока #{human.name} следующие карты:"
+    puts "У Игрока #{human.name} на руках:"
       human.hand.each { |card| puts "#{card.face} #{card.suit}"}
     puts
     show_player_points(human)
@@ -23,13 +23,13 @@ class Interface
 
   def show_comp_deal(comp, game_over)
     if game_over
-      puts "У Дилера следующие карты:"
+      puts "У Дилера на руках:"
       comp.hand.each { |card| puts "#{card.face} #{card.suit}"}
       puts
       show_dealer_points(comp)
     else
       puts
-      puts "У дилера такие карты:"
+      puts "У Дилера на руках:"
       puts
       (comp.hand.size).times { print '* ' }
       puts
@@ -40,5 +40,53 @@ class Interface
     puts "Выберите дальнейшее дейтсвие:"
     puts "1. - Взять еще карту 2. - Пропустить 3. - Вскрываемся"
     choice = gets.to_i
+  end
+
+  def show_dealer_hit
+    puts
+    puts "Дилер берет еще одну карту"
+    puts
+  end
+
+  def show_dealer_stay
+    puts
+    puts "Дилер пропускает ход"
+    puts
+  end
+
+  def show_game_over
+    puts
+    puts "Игра окончена со следующими результатами:"
+    puts
+  end
+
+  def show_bj_win
+    puts
+    puts "Black Jack!"
+    puts
+  end
+
+  def show_human_win(human)
+    puts
+    puts "Победил игрок #{human.name} с #{human.count_points} очками!"
+    puts
+  end
+
+  def show_comp_win(comp)
+    puts
+    puts "Победил Дилер с #{comp.count_points} очками!"
+    puts
+  end
+
+  def show_tie
+    puts
+    puts "Это ничья!"
+    puts
+  end
+
+  def show_lose
+    puts
+    puts "Перебор! Вы оба проиграли!"
+    puts
   end
 end
